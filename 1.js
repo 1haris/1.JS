64f80072e28456d5bd990378edeaa6e96d9295ef
1haris <56487554+1haris@users.noreply.github.com>
2019-10-26 12:10:58 +0700

    Create 1.js
    JSON pada REST API yaitu sebagai format untuk mentransmisikan (mengirim dan menerima) data dari REST API (server) ke client atau sebaliknya.

---
+++ 1.js
@@ -0,0 +1,24 @@
+function myBiodata(biodata){
+  let biodataObj = {
+    "name": 'Haris Setyawan',
+	"age": '22',
+    "address": 'Aceh',
+    "is_married": false,
+    "list_school": {
+      "university": 'Politeknik Negeri Lhokseumawe', year_in ('2015'), year_out ('2019'), major (salahudin)
+    },
+    "skill": [
+      {
+        "HTML5": "advanced",
+        "CSS": "beginner",
+        "Javascript": "advanced",
+        "C++": "advanced",
+        "Bootstrap": "advanced",
+        "PHP": "Advanced",
+      }
+    ]
+  }
+
+  return biodataObj
+}
+console.log(myBiodata('Haris Setyawan'));
